d58631882f548ad751cce9f639fb8e3095a3ae38
Sheetal <sheetal.singh458@gmail.com>
2022-06-08 14:42:24 +0530

    updated lab folder

--- src/database/db_config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DATABASE_NAME="comb"
-export DEBIAN_FRONTEND=noninteractive
-apt-get -q -y install mysql-server
-mysql -u root -Bse "create database $DATABASE_NAME;"
-cd ../src/database
-mysql -u root combustionandautomization mysql.sql
